a69ba8a7a29078492664c0a27dca41ea0fc6d4d8
沉静 <c.j.chenjing@foxmail.com>
2024-07-10 14:23:18 +0800

    feat: 修改获取位置

--- src/main/tool-package/check-hot-area.js
+++ src/main/tool-package/check-hot-area.js
@@ -1,8 +1,9 @@
 const { screen, Region } = require('@nut-tree-fork/nut-js')
 import { getCheckhotareaStatus, get_app_config } from './globals.js'
 
+const { a, b, t } = get_app_config()
+
 const checkHotArea = async () => {
-  const { a, b, t } = get_app_config()
   if (getCheckhotareaStatus()) {
     const height = await screen.height()
     await screen.highlight(new Region(a, t, b, height - 160))
