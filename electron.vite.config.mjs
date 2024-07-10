8779190d71143d22a1fc7f94c5691024a9216070
沉静 <c.j.chenjing@foxmail.com>
2024-07-10 11:05:47 +0800

    feat: dev和prod环境变量配置

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -16,5 +16,9 @@ export default defineConfig({
       }
     },
     plugins: [vue()]
+  },
+  // 根据环境变量设置
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
   }
 })
--- src/renderer/src/components/Versions.vue
+++ src/renderer/src/components/Versions.vue
@@ -1,18 +1,20 @@
 <script setup>
 import { ref, computed } from 'vue'
+
 const status = ref(false)
 window.electronAPI.onUpdateCounter((value) => {
   status.value = value
 })
 
+const nowenv = computed(() => {
+  return process.env.NODE_ENV
+})
+
 const showStatus = computed(() => {
   return status.value ? '运行中' : '未运行'
 })
-
 </script>
 
 <template>
-  <div class="running-status">
-    运行状态：{{ showStatus }}
-  </div>
+  <div class="running-status">运行状态：{{ showStatus }}, 当前环境：{{ nowenv }}</div>
 </template>
